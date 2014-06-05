e3ffca4db151f653e4faefb4b584e1a844b9efb2
Sam VB <you@example.com>
2014-06-04 22:18:03 -0400

    added errors to forms and got icons workng with accordion

--- client/js/sandbox.js
+++ client/js/sandbox.js
@@ -1,1 +1,9 @@
- 
+Meteor.startup(function(){
+	$(".accordtext a").click(function(event){
+		event.preventDefault();
+		$(this).siblings("div").first().slideToggle();
+		$(this).find("span").toggleClass("foundicon-plus");
+		$(this).find("span").toggleClass("foundicon-minus");
+		//return false;
+	});
+}); 
